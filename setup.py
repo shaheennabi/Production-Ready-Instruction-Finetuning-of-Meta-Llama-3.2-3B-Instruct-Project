d54ba9556b711781b9d35b7c7fb8a4ce1534b146
shaheennabi <ishaheennabi333@gmail.com>
2024-11-20 13:21:12 -0800

    setup added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="instruct_llama_finetuning",
+    version="0.0.1",
+    author="Shaheen Nabi",
+    author_email="ishaheenanbi333@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
