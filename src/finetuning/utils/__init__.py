66f3e3f9d6bfc5e516585fafa14437175fbeeee7
shaheennabi <ishaheennabi333@gmail.com>
2024-12-03 09:28:43 -0800

    utils updated

--- src/finetuning/utils/__init__.py
+++ src/finetuning/utils/__init__.py
@@ -1,10 +1,5 @@
 import yaml
 import os
-# Load configuration from the YAML file
-def load_config(file_path):
-    with open(file_path, 'r') as file:
-        config = yaml.safe_load(file)
-    return config
 
 
 # Load YAML configuration file
